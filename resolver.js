f693ad3528f32da43f0710838012b733e8279d0e
Ninapepite <killian.stein@icloud.com>
2023-10-07 22:59:02 +0200

    change schema for federation

--- resolver.js
+++ resolver.js
@@ -11,7 +11,7 @@ const RequestResponse = mongoose.model('RequestResponse', RequestResponseSchema)
 
 const resolvers = {
     Query: {
-        getRequestResponse: async (_, { location, personality }) => {
+        getCacheChat: async (_, { location, personality }) => {
             const entry = await RequestResponse.findOne({ location, personality });
             return entry ? entry.response : "Pas de réponse trouvée pour cette combinaison de localisation et de personnalité.";
         }
--- schema.js
+++ schema.js
@@ -1,27 +1,14 @@
 import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
-  type Query {
-    getRequestResponse(location: String!, personality: String!): String!
+  type CacheChat @key(fields: "id") {
+    id: ID!
+    location: String!
+    personality: String!
   }
 
-  # Pour la fédération, nous marquons notre type principal (par exemple RequestResponse, s'il était présent) avec @key 
-  # et indiquons quel champ est le champ clé.
-  # Dans cet exemple, je vais supposer que vous auriez un type RequestResponse avec un champ 'id' comme clé.
-  # type RequestResponse @key(fields: "id") {
-  #   id: ID!
-  #   location: String
-  #   personality: String
-  #   response: String
-  # }
-
-  # Ces types et champs supplémentaires sont nécessaires pour configurer la fédération
   extend type Query {
-    _service: _Service!
-  }
-
-  type _Service {
-    sdl: String
+    getCacheChat(location: String!, personality: String!): CacheChat!
   }
 `;
 
